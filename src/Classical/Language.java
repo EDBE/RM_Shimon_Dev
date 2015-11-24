2ba78b2dfa85e56f68a821fc14859290edd4f19a
Liang_Tang <ltangmt@gmail.com>
2015-11-23 23:45:31 -0500

    Change file/language/ comment all

--- src/Classical/Language.java
+++ src/Classical/Language.java
@@ -3,11 +3,11 @@ package Classical;
 /**
  * Created by musictechnology on 10/15/15.
  */
-public class Language {
-    public static void main(String args[]) {
-        String format = "(60000//$RT_TEMPO * 4 - 500)ms mnote PPITCH PDUR";
-
-        String nline = format.replaceAll("PPITCH", "88");
-        System.out.println(nline);
-    }
-}
+//public class Language {
+//    public static void main(String args[]) {
+//        String format = "(60000//$RT_TEMPO * 4 - 500)ms mnote PPITCH PDUR";
+//
+//        String nline = format.replaceAll("PPITCH", "88");
+//        System.out.println(nline);
+//    }
+//}
