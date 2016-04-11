77b43a3cfafbaed78114546f26893d98fd513718
Liang_Tang <ltangmt@gmail.com>
2016-04-11 01:20:36 -0400

    Integrating the Shimon play control part, and start to work on the Robot head control

--- src/ProjectStudioConcert/ProjectStudio.java
+++ src/ProjectStudioConcert/ProjectStudio.java
@@ -17,16 +17,30 @@ public class ProjectStudio extends MaxObject{
     int iDensityCounter4 = 0;
     int iPatternIdx = -1;
     int iRepetitionTimes = 1;
+    int iShimonHeadMoveHeight = -1;
+    int iSection1NodCount = 0;
+    int iSection2NodCount = 0;
+    int iSection3NodCount = 0;
+    int iSection4NodCount = 0;
+    int iBehaviorNodCount = 0;
+
+    float fHeadNodInterval = 1000.f;
 
     boolean bListen2NoteDensity = false;
     boolean bRhythmPatternIsDecided = false;
     boolean bListen2Drum = false;
+    boolean bShimonIsPlay = false;
 
-    Timer tTimer1 = new Timer();
+    Random rRandomGen = new Random();
+
+    Timer tTimer1 = new Timer();    //for prothetic arm
+    Timer tTimer2 = new Timer();    //for Shimon arm
+    Timer tHeadControl = new Timer();   // for Shimon Head Nodding
+    Timer tBasePanControl = new Timer();    //for Shimon base pan control
 
     public ProjectStudio() {
         declareInlets(new int[]{DataTypes.ALL, DataTypes.ALL});
-        declareOutlets(new int[]{DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL});
+        declareOutlets(new int[]{DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL, DataTypes.ALL});
         reset();
         System.out.println("Project Studio Object is updated!!!");
     }
@@ -35,12 +49,20 @@ public class ProjectStudio extends MaxObject{
         iSection = 0;
         iRepetitionTimes = 1;
         iPatternIdx = -1;
+        iShimonHeadMoveHeight = -1;
+        iSection1NodCount = 0;
+        iSection2NodCount = 0;
+        iSection3NodCount = 0;
+        iSection4NodCount = 0;
+        iBehaviorNodCount = 0;
 
         bListen2NoteDensity = false;
         bRhythmPatternIsDecided = false;
         bListen2Drum = false;
+        bShimonIsPlay = false;
 
         densityCounterClear();
+        headNodCounterClear();
 
 //        if (tTimer1 != null) {
 //            tTimer1.cancel();
@@ -51,21 +73,41 @@ public class ProjectStudio extends MaxObject{
         if (sec == 36 && iSection != 1) {
             iSection = 1;
             bListen2NoteDensity = false;
+            headNodCounterClear();
+            tHeadControl.schedule(new headmoveSection1(), 0);
+            System.out.println("We are in section " + iSection);
         } else if (sec == 38 && iSection != 2) {
             iSection = 2;
             bListen2NoteDensity = false;
+            headNodCounterClear();
+            tHeadControl.cancel();  //cancel the current execution
+            outlet(4, 0);   //prepare the midi file
+            outlet(9, 0);   //stop auto base pan
+            System.out.println("We are in section " + iSection);
         } else if (sec == 40 && iSection != 31) {
             iSection = 31;
             bListen2Drum = true;
+            headNodCounterClear();
+            tHeadControl.cancel();  //cancel the current execution
+            outlet(4, 1);
+            outlet(9, 0);   //stop auto base pan
+            System.out.println("We are in section " + iSection);
         } else if (sec == 37 && iSection != 4) {
             iSection = 4;
             bListen2NoteDensity = false;
+            headNodCounterClear();
+            tHeadControl.cancel();  //cancel the current execution
+            outlet(4, 2);
+            outlet(9, 0);   //stop auto base pan
+            System.out.println("We are in section " + iSection);
         } else if (sec == 45 && iSection != 32) {
             iSection = 32;
             bListen2NoteDensity = true;
+            headNodCounterClear();
+            tHeadControl.cancel();    //cancel the current execution
             outlet(1, "start 1024");
+            System.out.println("We are in section " + iSection);
         }
-        System.out.println("We are in section " + iSection);
     }
 
     /*
@@ -101,6 +143,36 @@ public class ProjectStudio extends MaxObject{
         tTimer1.schedule(new startPlayPattern(), 300);
     }
 
+    public void startShimon(int i) {
+        if (i == 50 && !bShimonIsPlay) {
+            tTimer2.schedule(new ShimonStartPlay(), 1000);
+            bShimonIsPlay = true;
+        }
+        if (iSection == 2) {
+            tHeadControl = new Timer();
+            tHeadControl.schedule(new headmoveSection2(), 1500);
+            tBasePanControl.schedule(new startAutoBasePan(), 1500+(int)(16*fHeadNodInterval));
+        } else if (iSection == 31) {
+            tHeadControl = new Timer();
+            tHeadControl.schedule(new headMoveSection3(), 1500);
+            tBasePanControl.schedule(new startAutoBasePan(), 1500+(int)(16*fHeadNodInterval));
+        } else if (iSection == 32) {
+            tHeadControl = new Timer();
+            tHeadControl.schedule(new headMoveSection3(), 500);
+        } else if (iSection == 4) {
+            tHeadControl = new Timer();
+            tHeadControl.schedule(new headMoveSection4(), 1500);
+            tBasePanControl.schedule(new startAutoBasePan(), 1500+(int)(16*fHeadNodInterval));
+        }
+    }
+
+    public void stopShimon(int i) {
+        if(bShimonIsPlay && i == 48) {
+            outlet(5, "stop");
+            bShimonIsPlay = false;
+        }
+    }
+
     public void bridge() {
         if (whichPattern() != -1) {
             outlet(0, whichPattern());
@@ -108,6 +180,51 @@ public class ProjectStudio extends MaxObject{
             startPlayRhythmPattern();
         } else {
             startPlayRhythmPattern();
+        }
+    }
+
+    public void ShimonFinishPlay() {
+        if (bShimonIsPlay) {
+            bShimonIsPlay = false;
+        }
+    }
+
+    public void keyboardVel(int vel) {
+        if (vel < 80 && iShimonHeadMoveHeight != 6) {
+            iShimonHeadMoveHeight = 6;
+        } else  if (vel >=80 & vel < 100 && iShimonHeadMoveHeight != 7) {
+            iShimonHeadMoveHeight = 7;
+        } else if(vel >= 100 && iShimonHeadMoveHeight != 8) {
+            iShimonHeadMoveHeight = 8;
+        }
+    }
+
+    public void stopALL() {
+        stopHeadMove();
+        stopArmPlay();
+        stopShimonPlay();
+    }
+
+    public void stopHeadMove() {
+        if (tBasePanControl != null) {
+            tBasePanControl.cancel();
+        }
+        if (tHeadControl != null) {
+            tHeadControl.cancel();
+        }
+    }
+
+    public void stopArmPlay() {
+        outlet(1, "stop");
+    }
+
+    public void stopShimonPlay() {
+        stopShimon(48);
+    }
+
+    public void stopByPad(int i) {
+        if (i == 41) {
+            stopALL();
         }
     }
 
@@ -140,9 +257,133 @@ public class ProjectStudio extends MaxObject{
         return max;
     }
 
+    private void headNodCounterClear() {
+        iSection1NodCount = 0;
+        iSection2NodCount = 0;
+        iSection3NodCount = 0;
+        iSection4NodCount = 0;
+        iBehaviorNodCount = 0;
+    }
+
     class startPlayPattern extends TimerTask {
         public void run() {
             outlet(1, "start 1024");
+        }
+    }
+
+    class ShimonStartPlay extends TimerTask {
+        public void run() {
+            outlet(5, "start 1024");
+        }
+    }
+
+    class headmoveSection1 extends TimerTask {
+        long waitTime;
+        public void run() {
+            if (iSection1NodCount <= 16) {
+                if (iSection1NodCount < 8) {
+                    outlet(10, 1.1f);
+                } else {
+                    outlet(10, -1.1f);
+                }
+                waitTime = (int)fHeadNodInterval;
+                outlet(6, fHeadNodInterval);    //HIP HOP NOD low
+                iSection1NodCount++;
+                tHeadControl.schedule(new headmoveSection1(), waitTime);
+            } else {
+                waitTime = (int)(fHeadNodInterval * 2);
+                outlet(10, 0);
+                tHeadControl.schedule(new headmoveSection1(), waitTime);
+            }
+        }
+    }
+    class headmoveSection2 extends TimerTask {
+        long waitTime;
+        public void run() {
+            if (iSection2NodCount <= 8) {
+                if (iSection2NodCount < 4) {
+                    outlet(10, 0.8f);
+                } else {
+                    outlet(10, -0.8f);
+                }
+                waitTime = (int)fHeadNodInterval;
+                outlet(6, fHeadNodInterval);    //HIP HOP NOD low
+                iSection2NodCount++;
+                tHeadControl.schedule(new headmoveSection2(), waitTime);
+            } else {
+                waitTime = (int)(fHeadNodInterval * 4);
+                outlet(10, 0);
+                tHeadControl.schedule(new headNodBehavior(), waitTime);
+            }
+        }
+    }
+    class headMoveSection3 extends TimerTask {//the height of the head depends on the velocity of the keyboard
+        long waitTime;
+        public void run() {
+            if (iSection3NodCount <= 16) {
+                if (iSection3NodCount < 8) {
+                    outlet(10, -1.1f);
+                } else {
+                    outlet(10, 1.1f);
+                }
+                waitTime = (int)fHeadNodInterval;
+                outlet(iShimonHeadMoveHeight, fHeadNodInterval);
+                iSection3NodCount++;
+                tHeadControl.schedule(new headMoveSection3(), waitTime);
+            }
+        }
+    }
+    class headMoveSection4 extends TimerTask {//the height of the head depends on the velocity of the keyboard
+        long waitTime;
+        public void run() {
+            if (iSection4NodCount <= 8) {
+                if (iSection4NodCount < 4) {
+                    outlet(10, 1.1f);
+                } else {
+                    outlet(10, -1.1f);
+                }
+                waitTime = (int)fHeadNodInterval;
+                outlet(iShimonHeadMoveHeight, fHeadNodInterval);
+                iDensityCounter4++;
+                tHeadControl.schedule(new headMoveSection4(), waitTime);
+            } else {
+                waitTime = (int) (fHeadNodInterval * 4);
+                if (tBasePanControl != null) {
+                    tBasePanControl.cancel();
+                }
+                outlet(10, 0);
+                tHeadControl.schedule(new headNodBehavior(), waitTime);
+            }
+        }
+    }
+    class headNodBehavior extends TimerTask {
+        public void run() {
+            if (rRandomGen.nextFloat()>.7f) {
+                float lookAtPosition = rRandomGen.nextFloat()*2 - 1.1f;
+                outlet(10, lookAtPosition);
+            }
+            if (iBehaviorNodCount < 10) {
+                outlet(8, fHeadNodInterval);
+            } else {
+                if (rRandomGen.nextFloat() > .7f) {
+                    float randNum = rRandomGen.nextFloat();
+                    if (randNum < .5f) {
+                        iShimonHeadMoveHeight = 6;
+                    } else if (randNum >= .5f && randNum<.85f) {
+                        iShimonHeadMoveHeight = 7;
+                    } else {
+                        iShimonHeadMoveHeight = 8;
+                    }
+                }
+                outlet(iShimonHeadMoveHeight, fHeadNodInterval);
+            }
+            iBehaviorNodCount++;
+            tHeadControl.schedule(new headNodBehavior(), (int)fHeadNodInterval);
+        }
+    }
+    class startAutoBasePan extends TimerTask {
+        public void run() {
+            outlet(9, 1);   //start the auto base pan
         }
     }
 
